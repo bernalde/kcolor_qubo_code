4dc9bfd37fa5ce99d7497ae3af1032d07fb89e6d
David Bernal <bernalde@cmu.edu>
2020-08-24 03:14:45 -0400

    Adding copy to dropbox command

---
+++ copy_to_dropbox.sh
@@ -0,0 +1,4 @@
+cp -r GAMS /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code
+cp -r MATLAB /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code
+cp results/* /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code/results/
+cp * /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code
