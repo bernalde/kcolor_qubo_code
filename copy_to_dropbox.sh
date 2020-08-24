1b2ac0321fc722258884d941dcdea941b47d737d
David Bernal <bernalde@cmu.edu>
2020-08-24 03:25:12 -0400

    Reorganizing files

--- copy_to_dropbox.sh
+++ copy_to_dropbox.sh
@@ -1,4 +1,7 @@
 cp -r GAMS /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code
 cp -r MATLAB /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code
 cp results/* /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code/results/
+cp results/mingap/* /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code/results/mingap/
+cp results/embedding/* /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code/results/embedding/
+cp results/dwave/* /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code/results/dwave/
 cp * /mnt/c/Users/debernal/Dropbox/Quantum\ Embedding/kcolor_qubo_code
