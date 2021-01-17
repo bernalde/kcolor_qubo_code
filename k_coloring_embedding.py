bf986d3c131f68e71aaaf60db648b86e1debb044
David Bernal <bernalde@cmu.edu>
2021-01-16 20:05:40 -0500

    Fixing new embedding results for k=2 p=0.75

--- k_coloring_embedding.py
+++ k_coloring_embedding.py
@@ -25,7 +25,7 @@ DRY_RUN = False
 
 
 def embedding(instance, TEST, k=2, prob=0.25, seed=42,
-              K=1, sampler='Advantage_system1.1'):
+              K=1, sampler='Advantage_system1.1', no_embed=False):
 
     # Store the results in csv files
     dir_path = os.path.dirname(os.path.abspath(__file__))
@@ -90,6 +90,9 @@ def embedding(instance, TEST, k=2, prob=0.25, seed=42,
         time_limit = 3600
         # Number of times the heuristic is run
         n_heur = 1000
+
+    if no_embed:
+        n_heur = 0
 
     columns = ['id', 'n_nodes', 'n_edges', 'nodes',
                'edges', 'alpha', 'target', 'density_target']
@@ -189,42 +192,43 @@ def embedding(instance, TEST, k=2, prob=0.25, seed=42,
                     min_length = X.number_of_nodes()
                     best_time = time_limit
                     best_embed = {}
-                    for n_h in range(n_heur):
-                        start = time.time()
-                        h_embed = mm.find_embedding(
-                            G, X, timeout=time_limit, random_seed=n_h)
-                        end = time.time()
-                        h_time = end - start
-                        h_times.append(h_time)
-                        # h_embeds.append(h_embed)
-                        count = 0
-                        for _, value in h_embed.items():
-                            count += len(value)
-                        tot_count += count
-                        if count == 0:
-                            fail += 1
-                        elif count < min_length:
-                            best_embed = h_embed
-                            min_length = count
-                            best_time = h_time
-                        h_lengths.append(count)
-                    succ = (n_heur - fail) / n_heur
-                    # temp['heur_embeds_' + ref] = h_embeds
-                    temp['heur_times_' + ref] = h_times
-                    temp['heur_lengths_' + ref] = h_lengths
-                    temp['heur_succ_' + ref] = succ
-                    if succ == 0:
-                        temp['heur_avgl_' + ref] = 'NaN'
-                        temp['heur_stdevl_' + ref] = 'NaN'
-                    else:
-                        avgl = tot_count / (succ * n_heur)
-                        temp['heur_avgl_' + ref] = avgl
-                        temp['heur_stdevl_' +
-                             ref] = statistics.stdev(h_lengths)
-                    temp['heur_avgt_' + ref] = np.median(h_times)
-                    temp['heur_best_embed_' + ref] = best_embed
-                    temp['heur_best_length_' + ref] = min_length
-                    temp['heur_best_time_' + ref] = best_time
+                    if not no_embed:
+                        for n_h in range(n_heur):
+                            start = time.time()
+                            h_embed = mm.find_embedding(
+                                G, X, timeout=time_limit, random_seed=n_h)
+                            end = time.time()
+                            h_time = end - start
+                            h_times.append(h_time)
+                            # h_embeds.append(h_embed)
+                            count = 0
+                            for _, value in h_embed.items():
+                                count += len(value)
+                            tot_count += count
+                            if count == 0:
+                                fail += 1
+                            elif count < min_length:
+                                best_embed = h_embed
+                                min_length = count
+                                best_time = h_time
+                            h_lengths.append(count)
+                        succ = (n_heur - fail) / n_heur
+                        # temp['heur_embeds_' + ref] = h_embeds
+                        temp['heur_times_' + ref] = h_times
+                        temp['heur_lengths_' + ref] = h_lengths
+                        temp['heur_succ_' + ref] = succ
+                        if succ == 0:
+                            temp['heur_avgl_' + ref] = 'NaN'
+                            temp['heur_stdevl_' + ref] = 'NaN'
+                        else:
+                            avgl = tot_count / (succ * n_heur)
+                            temp['heur_avgl_' + ref] = avgl
+                            temp['heur_stdevl_' +
+                                ref] = statistics.stdev(h_lengths)
+                        temp['heur_avgt_' + ref] = np.median(h_times)
+                        temp['heur_best_embed_' + ref] = best_embed
+                        temp['heur_best_length_' + ref] = min_length
+                        temp['heur_best_time_' + ref] = best_time
 
                     # start = time.time()
                     # Fully connected graph embedding happening here
@@ -264,14 +268,15 @@ if __name__ == "__main__":
     # graph_type = 'cycle'
     # graph_type = 'devil'
     graph_type = 'spreadsheet'
-    k = 5
+    k = 2
 
     TEST = True
+    no_embed = True
     prob = 0.75  # graph probability
-    K = 5
+    K = 4
 
-    # sampler = 'DW_2000Q_6'
-    sampler = 'Advantage_system1.1'
+    sampler = 'DW_2000Q_6'
+    # sampler = 'Advantage_system1.1'
 
     embedding(instance=graph_type, TEST=TEST, k=k,
-              prob=prob, K=K, sampler=sampler)
+              prob=prob, K=K, sampler=sampler, no_embed=no_embed)
